93bffef0a4e302b5646d6d730e2a4fe4ae9e9586
Marcio <marciogomes@alu.ufc.br>
2023-09-17 19:55:20 -0300

    work: instances of maxVal1 and randomNumbergenerator

--- lab.mjs
+++ lab.mjs
@@ -7,35 +7,29 @@ import {
 import maxValue2 from "./src/maxValue2.mjs";
 import maxValue from "./src/maxValue.mjs";
 
-const sizeInstance = 100000000;
-const targetValue = 99999126;
+/* 
+    100 
+    200 
+    1000 
+    2000  
+    5000 
+    10000 
+    50000 
+    100000 
+    500000 
+    1000000  <-
+    5000000   
+*/
+
+const sizeInstance = 100000;
+
+const generateRandomNumber = () => Math.floor(Math.random() * sizeInstance);
+
+const randomValue = generateRandomNumber(sizeInstance);
+
+const targetValue = randomValue;
 const iterations = 10;
 const note = `maxValue1_instância_${sizeInstance}_alvo_${targetValue}`;
-
-// loadData(`./data/data-for-search/ordered/${sizeInstance}.txt`).then(data =>{
-
-//     function algorithm(){
-
-//         // console.log("\n Busca Quadrática____________")
-//         // console.log(quadraticSearch(targetValue, data))
-
-//         // console.log("\n Busca Cubica____________")
-//         // console.log(cubicSearch(targetValue, data))
-
-//         // console.log("\n Busca Sequencial____________")
-//         // console.log(sequentialSearch_v2(targetValue, data))
-
-//         // console.log("\n Busca Binaria____________")
-//         // console.log(binarySearch(targetValue, data, 0, data.length))
-
-//         console.log("\n Busca Ternaria____________")
-//         console.log(ternarySearch(targetValue, data))
-
-//     }
-
-//     measureComplexity(algorithm, iterations, `${note}`)
-
-// });
 
 const data = generateUnorderedNumbers(sizeInstance);
 console.log(data);
